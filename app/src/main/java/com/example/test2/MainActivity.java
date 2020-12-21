63f9bf7ff09e694c3fe9b667b5caedef4f7c53d5
Mariana12 <mariananavarro3022@gmail.com>
2020-12-21 15:36:57 -0600

    new

---
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    LinearLayout listaView;
+    int num = 1;
+    ArrayList<TextView> lista = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.But);
+        listaView = (LinearLayout) findViewById(R.id.ListaView);
+    }
+
+    public void onClick(View v) {
+        TextView nodo  = new TextView(this);
+        nodo.setText(String.valueOf(num++));
+        nodo.setGravity(Gravity.CENTER);
+        nodo.setBackgroundResource(R.drawable.rectangle);
+        nodo.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        lista.add(nodo);
+        listaView.addView(nodo);
+
+    }
+}
